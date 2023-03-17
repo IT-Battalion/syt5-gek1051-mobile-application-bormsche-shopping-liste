61916ff8ba7158865eb5302530aa740542cf5649
jhochsteger <jhochsteger@student.tgm.ac.at>
2023-03-17 11:06:03 +0100

    Moved svelte to frontend folder
    for a better structure of the git repo

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+	import logo from '../lib/images/svelte-logo.svg';
+	import github from '../lib/images/github.svg';
 </script>
 
 <header>
@@ -17,7 +17,7 @@
 		</svg>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+				<a href="/static">Home</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
