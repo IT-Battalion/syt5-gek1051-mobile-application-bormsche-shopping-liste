7ddc57d32a679421a17f9a7c1382fbf07d6356e0
Patrick Elias <47031872+pelias006@users.noreply.github.com>
2023-04-08 14:39:00 +0200

    Add: Mark as bought test in order to test if a item can be set as bought

--- frontend/cypress/e2e/frontend/shoppinglist_entry.cy.js
+++ frontend/cypress/e2e/frontend/shoppinglist_entry.cy.js
@@ -9,7 +9,16 @@ describe('shopping list entry', () => {
     cy.get('#add_item_input').type('test item 123')
     cy.get('#add_item_btn').click();
     cy.get('#item_list').children().should('have.length.above', 1)
+    cy.wait(500)
   })
+
+  it('mark as bought', () => {
+    cy.get('#0_add').click()
+    cy.get('#item_list').children().should('have.length.above', 1)
+    cy.get('#0_item_check').check({force:true})
+    cy.get('#0_item_check').should('have.value', 'true')
+    cy.wait(500)
+  });
 
   it('delete item', () => {
     cy.get('#0_add').click()
